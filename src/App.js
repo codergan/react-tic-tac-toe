245f721d70f9abc367b86bf3258d1ba437dd0b10
Ethan <ganx0005@e.ntu.edu.sg>
2019-07-03 18:48:21 +0800

    The small interesting game

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to React !!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href = "https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/es6.js
@@ -0,0 +1,22 @@
+// const obj={name:'Ethan', age:18}
+// console.log(Object.entries(obj))
+
+
+
+
+// function hello(name){
+// 	console.log(`hello ${name} !`)
+// }
+// hello('Ethan')
+
+// const hello1=(name)=>{
+// 	console.log(`hello ${name} !`)
+// }
+// hello1('Vanilla')
+
+// setTimeout (()=>{
+// 		console.log('arrow function')
+// 	},1000)
+
+// const double = x=>x*2
+// console.log(double(66))
